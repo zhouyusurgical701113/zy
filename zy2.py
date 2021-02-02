3560e86051fe50af51f514a24f830dcb75e1dab7
zhouyusurgical701113 <yu.zhou@bausch.com>
2021-02-02 17:50:30 +0800

    zy1.py

---
+++ zy2.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app=Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+def show_user_profile(username):
+    return render_template('user.html',name=username)
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+# zy
